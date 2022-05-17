6f2ca08eb319c4d38309d6d74a0e3c6075056ad6
fifisaad <68048261+fifisaad@users.noreply.github.com>
2022-05-17 08:34:51 +0200

    Update2

--- main.h
+++ main.h
@@ -2,9 +2,7 @@
 #define _FUNCTION_PRINTF_H_
 #include <stdarg.h>
 #include <stdlib.h>
-#include <stddef.h>
 #include <stdio.h>
-#define BUFFER_SIZE 1024
 
 /**
  * struct modifier - mofifier fields collection
@@ -26,46 +24,7 @@ typedef struct modifier
 	char specifier;
 } modifier_t;
 
-void *_realloc(void *ptr, unsigned int, unsigned int);
 int _putchar(char c);
-char *print_binary(modifier_t *, va_list);
-char *print_unsigned_int(modifier_t *, va_list);
-char *print_octal(modifier_t *, va_list);
-char *print_hex(modifier_t *, va_list);
-char *print_char(modifier_t *, va_list ap);
-char *print_int(modifier_t *, va_list ap);
-char *print_string(modifier_t *modif, va_list ap);
-char *print_rev(modifier_t *, va_list ap);
-char *print_big_s(modifier_t *, va_list);
-char *print_pointer(modifier_t *, va_list);
-char *rot13(char *s);
-int _strlen(char *s);
-char *print_rot(modifier_t *, va_list ap);
-
-void free_modifier(modifier_t *);
-char *get_flags(const char *, unsigned int *);
-int get_width(const char *, unsigned int *);
-int get_precision(const char *, unsigned int *);
-char *get_length(const char *, unsigned int *);
-char get_specifier(const char *, unsigned int *);
-modifier_t *get_modifier(const char *, unsigned int *);
-char *treat_format(const char *, unsigned int *, va_list);
 int _printf(const char *format, ...);
-char *_strcpy(char *dest, char *src);
-void array_rev(char *arr, int len);
-int int_len(int num);
-char *ito(int n);
-char *reverse(char *s);
-
-/**
- * struct print - multiple choice print
- * @f: char Type of print
- * @func: funct
- */
-typedef struct print
-{
-	char f;
-	char *(*func)(modifier_t *, va_list);
-} t_print;
 
 #endif
