3cdc2034490885c131fd0e6c744110a63131081a
fifisaad <68048261+fifisaad@users.noreply.github.com>
2022-05-18 09:50:27 +0200

    Update2_main

--- main.h
+++ main.h
@@ -1,22 +1,39 @@
-#ifndef my_printf
-#define my_printf
-#include <stdio.h>
 #include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <stddef.h>
 
 /**
- * struct specifier - struct specifier
- * @valid: the valid character.
- * @f: the functions associated.
- *
+ * _printf - recreates the printf function
+ * @format: string with format specifier
+ * Return: number of characters printed
  */
-typedef struct specifier
+
+
+int _printf(const char * format, ...)
 {
-	char *valid;
-	void(*f)();
-} spec;
-int _printf(const char *format, ...);
-int print_c(va_list list);
-int print_s(va_list list);
-int print_percent(va_list list);
-int _putchar(char c);
-#endif
+	int i, p;
+	va_list args;
+	void (*f)(va_list);
+
+	va_start(args, format);
+	i = 0;
+	while (format != NULL && format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			f = get_func(format[i + 1]);
+			p = f(args);	
+
+
+
+
+		}
+
+			i++;
+
+	}
+	va_end(args);
+	return(0);
+}
